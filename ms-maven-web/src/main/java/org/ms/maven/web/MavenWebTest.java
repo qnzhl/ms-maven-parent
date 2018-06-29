f23ca71410a141f470267f7c7a8320f4b8cb8199
qnzhl <qnzhl@163.com>
2018-06-29 23:27:12 +0800

    init local git1 commit

--- ms-maven-web/src/main/java/org/ms/maven/web/MavenWebTest.java
+++ ms-maven-web/src/main/java/org/ms/maven/web/MavenWebTest.java
@@ -7,6 +7,7 @@ public class MavenWebTest {
 	public static void main(String[] args) {
 		MavenService service=new MavenService();
 		service.mavenServiceTest();
+		System.out.println("测试git1修改！");
 	}
 
 }
